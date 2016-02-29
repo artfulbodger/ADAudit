470ca60d9955284b1c73465dc0b2a6bad847d8ca
artfulbodger <Richard.Carpenter@G112340WIN10.thisisglobal.com>
2016-02-29 14:07:35 +0000

    Added Send-ReportEmail

--- ADAudit/ADAudit.ps1
+++ ADAudit/ADAudit.ps1
@@ -198,3 +198,83 @@ function Remove-ADSession
     {
     }
 }
+
+<#
+.Synopsis
+   Short description
+.DESCRIPTION
+   Long description
+.EXAMPLE
+   Example of how to use this cmdlet
+.EXAMPLE
+   Another example of how to use this cmdlet
+#>
+function Send-ReportEmail
+{
+    [CmdletBinding()]
+    [Alias()]
+    [OutputType([int])]
+    Param
+    (
+        # Data table to be rendered in report
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=0)]
+        $bodydata,
+
+        # Report text that appears before data table
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=1)]
+        $bodytext,
+
+        # SMTP server for sending Mail
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=2)]
+        $SmtpServer,
+
+        # From Email Address
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=3)]
+        $FromEmailAddress,
+
+        # To Email Address
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=4)]
+        $ToEmailAddress,
+
+        # Subject
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=5)]
+        $EmailSubject
+    )
+
+    Begin
+    {
+        $style = $null
+        $style = "<style>BODY{font-family: Arial; font-size: 10pt;}"
+        $style = $style + "TABLE{border: 1px solid black; border-collapse: collapse;}"
+        $style = $style + "TH{border: 1px solid black; background: #2f84c6; padding: 5px; color: #fff }"
+        $style = $style + "TD{border: 1px solid black; padding: 5px; } .red { background-color: #f00;}"
+        $style = $style + "</style>"
+    }
+    Process
+    {
+        
+
+        $bodydatahtml = $bodydata | ConvertTo-Html -Head $style
+
+        $body = "<p>$bodytext</p><p>$bodydatahtml</p>"
+        #$body += $bodydatahtml
+
+        Send-MailMessage -SmtpServer $SmtpServer -From $FromEmailAddress -to $ToEmailAddress -Subject $EmailSubject -BodyAsHtml -Body "$body"
+    }
+    End
+    {
+        
+    }
+}
