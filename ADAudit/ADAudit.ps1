53faec6898d2911716523cc144c9cc7df32fc36a
artfulbodger <Richard.Carpenter@G112340WIN10.thisisglobal.com>
2016-02-29 14:04:05 +0000

    Added Get-Linemanager, Get-ADSession and RemoveADSession functions

--- ADAudit/ADAudit.ps1
+++ ADAudit/ADAudit.ps1
@@ -75,3 +75,126 @@ function Get-StaleUserReport
     }
 }
 
+function Get-LineManager
+{
+    [CmdletBinding()]
+    [OutputType([int])]
+    Param
+    (
+        # User samAccountName
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=0)]
+        $user
+    )
+
+    Begin
+    {
+        Get-ADSession -domainController 'dsdc2.thisisglobal.com'
+    }
+    Process
+    {
+        $tempuser = Get-RMADUser -Identity $user -Properties manager
+        If($tempuser.manager -ne $null){
+            $lm = Get-RMADUser -Identity $tempuser.manager
+            Return $lm
+        } else {
+            Return $null
+        }
+    }
+    End
+    {
+    }
+}
+
+<#
+.Synopsis
+   Creates a new PSSession to A Domain Controller
+.DESCRIPTION
+   Long description
+.EXAMPLE
+   Get-ADSession -domainController 'dc1.contoso.com'
+#>
+function Get-ADSession
+{
+    [CmdletBinding()]
+    [Alias()]
+    [OutputType([int])]
+    Param
+    (
+        # Domain Controller to use
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=0)]
+        $domainController
+    )
+
+    Begin
+    {
+    }
+    Process
+    {
+        $adsession = $false
+        $remoteSessionList = Get-PSSession -ComputerName $domainController
+        Foreach($remoteSession in $remoteSessionList) {
+            If($remoteSession.Name -eq 'ActiveDirectory'){
+                $adsession = $true
+                break
+            }
+        }
+
+        If($adsession -eq $false){
+            $Session = New-PSsession -Computername $dc -Name ActiveDirectory
+            # Use the newly created remote Powershell session to send a #command to that session
+            Invoke-Command -Command {Import-Module ActiveDirectory} -Session $Session
+            # Use that session with the modules to add the available # commandlets to your existing Powershell command shell with a #new command name prefix.
+            Import-PSSession -Session $Session -Module ActiveDirectory -Prefix RM -AllowClobber
+        }
+
+    }
+    End
+    {
+    }
+}
+
+<#
+.Synopsis
+   Short description
+.DESCRIPTION
+   Long description
+.EXAMPLE
+   Example of how to use this cmdlet
+.EXAMPLE
+   Another example of how to use this cmdlet
+#>
+function Remove-ADSession
+{
+    [CmdletBinding()]
+    [Alias()]
+    [OutputType([int])]
+    Param
+    (
+        # Domain Controller to use
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=0)]
+        $domainController
+    )
+
+    Begin
+    {
+    }
+    Process
+    {
+        $remoteSessionList = Get-PSSession -ComputerName $domainController
+        Foreach($remoteSession in $remoteSessionList) {
+            If($remoteSession.Name -eq 'ActiveDirectory'){
+                Remove-PSSession -Name ActiveDirectory
+                break
+            }
+        }
+    }
+    End
+    {
+    }
+}
