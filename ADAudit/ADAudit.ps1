92ac628b0d6536de2fd59f6fe6950afeb97b8aac
artfulbodger <Richard.Carpenter@G112340WIN10.thisisglobal.com>
2016-03-01 22:01:57 +0000

    Added Get-ADSchemaVersion Function

--- ADAudit/ADAudit.ps1
+++ ADAudit/ADAudit.ps1
@@ -180,6 +180,104 @@ function Get-PasswordNeverExpires
     }
 }
 
+<#
+.Synopsis
+   Short description
+.DESCRIPTION
+   Long description
+.EXAMPLE
+   Example of how to use this cmdlet
+.EXAMPLE
+   Another example of how to use this cmdlet
+#>
+function Get-ADSchemaVersions
+{
+    [CmdletBinding()]
+    [Alias()]
+    [OutputType([int])]
+    Param
+    (
+        # Domain Controller to use
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=0)]
+        $dc
+    )
+
+    Begin
+    {
+        Get-ADSession -domainController $dc
+        $SchemaVersions = @()
+        $SchemaHashAD = @{ 
+            13="Windows 2000 Server"; 
+            30="Windows Server 2003 RTM"; 
+            31="Windows Server 2003 R2"; 
+            44="Windows Server 2008 RTM"; 
+            47="Windows Server 2008 R2"; 
+            56="Windows Server 2012 RTM"; 
+            69="Windows Server 2012 R2" 
+        }
+
+        $SchemaHashExchange = @{ 
+            4397="Exchange Server 2000 RTM"; 
+            4406="Exchange Server 2000 SP3"; 
+            6870="Exchange Server 2003 RTM"; 
+            6936="Exchange Server 2003 SP3"; 
+            10628="Exchange Server 2007 RTM"; 
+            10637="Exchange Server 2007 RTM"; 
+            11116="Exchange 2007 SP1"; 
+            14622="Exchange 2007 SP2 or Exchange 2010 RTM"; 
+            14625="Exchange 2007 SP3"; 
+            14726="Exchange 2010 SP1"; 
+            14732="Exchange 2010 SP2"; 
+            14734="Exchange 2010 SP3"; 
+            15137="Exchange 2013 RTM";
+            15292="Exchange 2013 SP1"
+        } 
+    }
+    Process
+    {
+        $SchemaPartition = (Get-RMADRootDSE).NamingContexts | Where-Object {$_ -like "*Schema*"} 
+        $SchemaVersionAD = (Get-RMADObject $SchemaPartition -Property objectVersion).objectVersion 
+        $SchemaVersions += 1 | Select-Object @{name="Product";expression={"AD"}}, @{name="Schema";expression={$SchemaVersionAD}}, @{name="Version";expression={$SchemaHashAD.Item($SchemaVersionAD)}} 
+        
+        $SchemaPathExchange = "CN=ms-Exch-Schema-Version-Pt,$SchemaPartition" 
+        #If (Test-Path "AD:$SchemaPathExchange") { 
+            $SchemaVersionExchange = (Get-RMADObject $SchemaPathExchange -Property rangeUpper).rangeUpper 
+        #} Else { 
+        #    $SchemaVersionExchange = 0 
+        #} 
+        $SchemaVersions += 1 | Select-Object @{name="Product";expression={"Exchange"}}, @{name="Schema";expression={$SchemaVersionExchange}}, @{name="Version";expression={$SchemaHashExchange.Item($SchemaVersionExchange)}} 
+        
+
+        $SchemaHashLync = @{ 
+    1006="LCS 2005"; 
+    1007="OCS 2007 R1"; 
+    1008="OCS 2007 R2"; 
+    1100="Lync Server 2010"; 
+    1150="Lync Server 2013" 
+    } 
+ 
+$SchemaPathLync = "CN=ms-RTC-SIP-SchemaVersion,$SchemaPartition" 
+If (Test-Path "AD:$SchemaPathLync") { 
+    $SchemaVersionLync = (Get-ADObject $SchemaPathLync -Property rangeUpper).rangeUpper 
+} Else { 
+    $SchemaVersionLync = 0 
+} 
+ 
+$SchemaVersions += 1 | Select-Object @{name="Product";expression={"Lync"}}, @{name="Schema";expression={$SchemaVersionLync}}, @{name="Version";expression={$SchemaHashLync.Item($SchemaVersionLync)}} 
+ 
+
+
+
+        $SchemaVersions | Format-Table * -AutoSize 
+    }
+    End
+    {
+        Remove-ADSession -domainController $dc
+    }
+}
+
 function Get-LineManager
 {
     [CmdletBinding()]
